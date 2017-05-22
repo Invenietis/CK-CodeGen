2f7f0a1b806f9653f3aa0286cf9a45f38952ff06
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-05-22 12:47:45 +0200

    Added ToSourceString.

---
+++ CK.CodeGen/Modifiers/CodeGenExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CK.CodeGen
+{
+    public static class CodeGenExtensions
+    {
+        static public string ToSourceString(this string @this)
+        {
+            return $"@\"{@this.Replace("\"", "\"\"")}\"";
+        }
+
+    }
+
+
+}
