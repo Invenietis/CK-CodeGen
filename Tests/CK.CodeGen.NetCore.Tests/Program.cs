46488d7fff8b03bf242c15c85e7d21acc496920a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-10-20 22:44:44 +0200

    Welcome CK-World!

--- Tests/CK.CodeGen.NetCore.Tests/Program.cs
+++ Tests/CK.CodeGen.NetCore.Tests/Program.cs
@@ -1,13 +1,18 @@
 using NUnitLite;
+using System;
 using System.Globalization;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace CK.StObj.Engine.Tests.NetCore
 {
     public static class Program
     {
+        static string TestPathMap( [CallerFilePath]string p = null ) => p;
+
         public static int Main(string[] args)
         {
+            Console.WriteLine( TestPathMap() );
             CultureInfo.CurrentCulture
                 = CultureInfo.CurrentUICulture
                 = CultureInfo.DefaultThreadCurrentCulture
