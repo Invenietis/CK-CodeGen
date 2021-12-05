35482fe522274c2f89b8ed5ce17659c853a4622a
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-12-05 10:24:18 +0100

    Handles withNamespaces parameter in ToCSharpName.

--- CK.CodeGen/ExternalTypeExtensions.cs
+++ CK.CodeGen/ExternalTypeExtensions.cs
@@ -20,9 +20,9 @@ namespace CK.CodeGen
         /// </summary>
         /// <param name="this"></param>
         /// <returns>True if this is a value tuple. False otherwise.</returns>
-        public static bool IsValueTuple( this Type @this )
+        public static bool IsValueTuple( this Type? @this )
         {
-            return @this.Namespace == "System" && @this.Name.StartsWith( "ValueTuple`", StringComparison.Ordinal );
+            return @this != null && @this.Namespace == "System" && @this.Name.StartsWith( "ValueTuple`", StringComparison.Ordinal );
         }
 
         /// <summary>
@@ -51,18 +51,6 @@ namespace CK.CodeGen
             return @this == null
                         ? "null"
                         : $"@\"{@this.Replace( "\"", "\"\"", StringComparison.Ordinal )}\"";
-        }
-
-        /// <summary>
-        /// See <see cref="CodeWriterExtensions.AppendCSharpName{T}(T, Type, bool, bool)"/>.
-        /// </summary>
-        /// <param name="this">This type.</param>
-        /// <param name="typeDeclaration">True to include generic parameter names in the output.</param>
-        /// <param name="useValueTupleParentheses">False to use the (safer) "System.ValueTuple&lt;&gt;" instead of the (tuple, with, parentheses, syntax).</param>
-        /// <returns>The C# type name.</returns>
-        public static string ToCSharpName( this Type? @this, bool typeDeclaration = true, bool useValueTupleParentheses = true )
-        {
-            return new StringCodeWriter().AppendCSharpName( @this, typeDeclaration, useValueTupleParentheses ).ToString();
         }
     }
 }
